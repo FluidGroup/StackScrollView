0196abfbe64ef236dde20816af893fe95fae56e7
muukii <m@muukii.me>
2016-09-18 01:27:08 +0900

    🤔WIP [ci skip]

---
+++ StackScrollView/StackScrollView.h
@@ -0,0 +1,19 @@
+//
+//  StackScrollView.h
+//  StackScrollView
+//
+//  Created by muukii on 9/18/16.
+//  Copyright © 2016 muukii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StackScrollView.
+FOUNDATION_EXPORT double StackScrollViewVersionNumber;
+
+//! Project version string for StackScrollView.
+FOUNDATION_EXPORT const unsigned char StackScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StackScrollView/PublicHeader.h>
+
+
