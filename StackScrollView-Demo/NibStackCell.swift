6d079d97af45d2cd02de50db9140f14831e3137e
muukii <m@muukii.me>
2017-05-17 04:23:30 +0900

    Add NibLoader

---
+++ StackScrollView-Demo/NibStackCell.swift
@@ -0,0 +1,14 @@
+//
+//  NibStackCell.swift
+//  StackScrollView
+//
+//  Created by muukii on 5/17/17.
+//  Copyright © 2017 muukii. All rights reserved.
+//
+
+import UIKit
+
+final class NibStackCell: UIView {
+  
+  @IBOutlet weak var slider: UISlider!
+}
